965be9bf9206b546f028bd8d1cdfa2a64fe20c86
Gauravrawat2001 <rawatgauravsingh588@gmail.com>
2023-03-27 23:45:48 +0530

    code added

--- 01.Arrays and Strings/14.SpiralMatrix.java
+++ 01.Arrays and Strings/14.SpiralMatrix.java
@@ -1,41 +1,44 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+      
 
         int fr = 0, lr = matrix.length - 1, fc = 0, lc = matrix[0].length - 1;
-        int count = 0;
+      
 
-        List<Integer> res = new ArrayList<>();
+        List<Integer> res = new ArrayList<Integer>();
     
-        while(count <= rows * cols){
+        while(fr <= lr && fc <= lc){
 
             // for first row 
             for(int i = fc ; i <= lc; i++){
                 res.add(matrix[fr][i]);
-                count++;
+                
             }
             fr++;
 
             // for last colmn
             for(int i = fr; i <= lr; i++){
-                res.add(matrix[lc][i]);
-                count++;
+                res.add(matrix[i][lc]);
+               
             }
             lc--;
 
             // for last row 
+            if(fr <= lr){
             for(int i = lc ; i >= fc; i--){
                 res.add(matrix[lr][i]);
-                count++;
+               
             }
+        }
             lr--;
 
             // for first column
+            if (fc <= lc){
             for(int i = lr ; i >= fr; i--){
                 res.add(matrix[i][fc]);
-                count++;
+                
             }
+        }
             fc++;
         }
         return res;
