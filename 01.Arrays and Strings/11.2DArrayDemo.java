a3157c2d287942696745d3489286272ad7e26586
Gauravrawat2001 <rawatgauravsingh588@gmail.com>
2023-03-27 10:19:14 +0530

    code added

--- 01.Arrays and Strings/11.2DArrayDemo.java
+++ 01.Arrays and Strings/11.2DArrayDemo.java
@@ -4,24 +4,25 @@ import java.util.*;
 class Solution{
 
 public static void main(String[] args) throws Exception {
-    // write your code here
-    Scanner scn = new Scanner(System.in);
-    int row = scn.nextInt();
-    int col = scn.nextInt();
-    
-    int[][] arr = new int[row][col];
-    
-    for(int i = 0; i < arr.length; i++){
-        for(int j = 0; j < arr[0].length; j++){
-            arr[i][j] = scn.nextInt();
+    try (// write your code here
+    Scanner scn = new Scanner(System.in)) {
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+        
+        int[][] arr = new int[row][col];
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = scn.nextInt();
+            }
         }
-    }
-    
-    for(int i = 0; i < arr.length; i++){
-        for(int j = 0; j < arr[0].length; j++){
-            System.out.print(arr[i][j] +"\t");
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] +"\t");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
  }
 }
