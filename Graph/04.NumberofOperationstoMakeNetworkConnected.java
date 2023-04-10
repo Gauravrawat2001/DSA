e8e8ed2d4d1a9c82bddcaad8abd53bfc48f9b208
Gauravrawat2001 <rawatgauravsingh588@gmail.com>
2023-04-11 00:03:17 +0530

    code added

--- Graph/04.NumberofOperationstoMakeNetworkConnected.java
+++ Graph/04.NumberofOperationstoMakeNetworkConnected.java
@@ -1,4 +1,58 @@
 package Graph;
 
+import java.util.ArrayList;
+
 // Leetcode - > 1319
 
+class Graph{
+    int n; //  number of vertices
+    ArrayList<Integer>[] adj;
+    
+    Graph(int n){
+        this.n = n;
+        // adj = new ArrayList[n];
+        for(int i = 0; i < n; i++){
+            adj[i] = new ArrayList<>();
+        }
+        
+    }
+    // unweighted , directed
+    public void addEdge(int src, int dest){
+        adj[src].add(dest);
+        adj[dest].add(src); //Gr runs only when graph is undirected
+    }
+
+    public void dfs(int src , boolean[] vis){
+        if(vis[src] == true) return;
+        
+        vis[src] = true;
+        for(int nbr : adj[src])
+             dfs(nbr , vis);
+    }
+    
+   
+    
+}
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if(n - 1 > connections.length) return -1;
+
+        Graph g = new Graph(n);
+        for(int[] edge : connections){
+            g.addEdge(edge[0] , edge[1]);
+        }
+
+        boolean[] vis = new boolean[n];
+        int components = 0;
+        for(int i = 0; i < n; i++){
+            if(vis[i] == true) continue;
+            components++;
+            g.dfs(i, vis);
+        }
+
+        return components - 1;
+    }
+
+    public Solution() {
+    }
+}
---
+++ Graph/05.MinimumSwapstoSort.java
@@ -0,0 +1,69 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+// GFG 
+class Graph{
+    int n; //  number of vertices
+    ArrayList<Integer>[] adj;
+    
+    Graph(int n){
+        this.n = n;
+        //  adj = new ArrayList[n];
+        for(int i = 0; i < n; i++){
+            adj[i] = new ArrayList<>();
+        }
+        
+    }
+    // unweighted , directed
+    public void addEdge(int src, int dest){
+        adj[src].add(dest);
+        adj[dest].add(src); //Gr runs only when graph is undirected
+    }
+
+    public int dfs(int src , boolean[] vis){
+        if(vis[src] == true) return 0;
+        
+        vis[src] = true;
+        int size = 1;
+        for(int nbr : adj[src])
+             size += dfs(nbr , vis);
+        return size;         
+    }
+}
+
+class Solution
+{
+    //Function to find the minimum number of swaps required to sort the array.
+  
+    public int minSwaps(int nums[])
+    {
+        // Code here
+        HashMap<Integer , Integer> oldIdx = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            oldIdx.put(nums[i] , i);
+        }
+        
+        Arrays.sort(nums);
+        Graph g = new Graph(nums.length);
+        for(int i = 0; i < nums.length; i++){
+            int j = oldIdx.get(nums[i]);
+            g.addEdge(i , j);
+        }
+        
+        int swaps = 0;
+        boolean[] vis = new boolean[nums.length];
+        for(int i = 0 ; i < nums.length; i++){
+            if(vis[i] == true) continue;
+           swaps += g.dfs(i , vis) - 1;
+            // swaps += (size - 1);
+            
+        }
+        
+        return swaps;
+        
+    }
+}
+
