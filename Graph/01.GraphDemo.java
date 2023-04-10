e7dc8bf0b84dbc54774c143d0f62f5ee172cb847
Gauravrawat2001 <rawatgauravsingh588@gmail.com>
2023-04-10 20:57:01 +0530

    code added

---
+++ Graph/01.GraphDemo.java
@@ -0,0 +1,47 @@
+package Graph;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// "static void main" must be defined in a public class.
+class Graph{
+    int n; //  number of vertices
+    ArrayList<Integer>[] adj;
+    
+    Graph(int n){
+        this.n = n;
+        adj = new ArrayList[n];
+        for(int i = 0; i < n; i++){
+            adj[i] = new ArrayList<>();
+        }
+        
+    }
+    // unweighted , directed
+    public void addEdge(int src, int dest){
+        adj[src].add(dest);
+        adj[dest].add(src); //Gr runs only when graph is undirected
+    }
+    
+   
+    
+}
+ class Main {
+    public static void main(String[] args) {
+       try (Scanner scn = new Scanner(System.in)) {
+        int n = scn.nextInt();
+        
+        Graph g = new Graph(n);
+        
+        int e = scn.nextInt();
+        while(e-- > 0){
+            int src = scn.nextInt();
+            int dest = scn.nextInt();
+            g.addEdge(src , dest);
+        }
+        
+       for(int i = 0; i < n; i++){
+           System.out.println(g.adj[i]);
+       }
+    }
+        
+    }
+}
