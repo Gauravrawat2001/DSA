e103bb7c611c0f673aba2c011079b25ec75d5e2e
Gauravrawat2001 <rawatgauravsingh588@gmail.com>
2023-06-13 23:23:16 +0530

    code added

---
+++ 02.BinarySearch/02.FirstTransitionPoint.java
@@ -0,0 +1,24 @@
+// GFG
+
+class GfG {
+    int transitionPoint(int arr[], int n) {
+        // code here
+        if(arr[n - 1] == 0) return -1;
+        
+        int left = 0 , right = arr.length - 1;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+            if(arr[mid] == 0){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        
+        return left;
+    }
+}
+
+
