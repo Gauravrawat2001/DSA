24c41fe954f96c31384d7387e274fbc13971e1a3
Gauravrawat2001 <rawatgauravsingh588@gmail.com>
2023-07-27 21:08:26 +0530

    code added

--- 04.SortingAlgorithms/10.SortAnArray.java
+++ 04.SortingAlgorithms/10.SortAnArray.java
@@ -48,3 +48,58 @@
         return mergeSort(nums , 0 , nums.length - 1);
  }
  }
+
+
+ // Method 2 -> No return type method
+
+ 
+  
+ class Solution2 {
+    // Recursive Code (Divide and Conquer)
+     public void mergeSort(int[]nums , int l , int r){
+          if(l >= r) return ;
+          
+         int mid = (l + r)/2;
+         mergeSort(nums, l, mid);
+         mergeSort(nums, mid + 1, r);
+ 
+         merge(nums , l , mid , mid + 1, r);
+     }
+ 
+     public void merge(int[] nums ,int l1 , int r1 , int l2 , int r2 ){
+         int[] res = new int[r2 - l1 + 1];
+         int p1 = l1, p2 = l2, p3 = 0;
+ 
+         while(p1 <= r1 && p2 <= r2){
+             if(nums[p1] <= nums[p2]){
+                 res[p3] = nums[p1];
+                 p3++;
+                 p1++;
+             }else{
+                 res[p3] = nums[p2];
+                 p2++;
+                 p3++;
+             }
+         }
+ 
+         while(p1 <= r1){
+             res[p3] = nums[p1];
+                 p3++;
+                 p1++;
+         }
+ 
+         while(p2 <= r2){
+             res[p3] = nums[p2];
+                 p2++;
+                 p3++;
+         }
+ 
+         for(int idx = l1; idx <= r2; idx++){
+             nums[idx] = res[idx - l1];
+         }
+     }
+     public int[] sortArray(int[] nums) {
+        mergeSort(nums , 0 , nums.length - 1);
+        return nums;
+ }
+ }
